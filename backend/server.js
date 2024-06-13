58f46e079b380ce99246af956f90ad5c0429beaa
MrMohamedRizwan <109430288+MrMohamedRizwan@users.noreply.github.com>
2024-06-13 12:09:09 +0530

    seperate backend

--- backend/server.js
+++ backend/server.js
@@ -52,23 +52,23 @@ app.use('/api/message',messageRoutes)
 
 ///deploy
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-  // app.get("/", (req, res) => {
-  //   res.send("API is running..");
-  // });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-///deploy
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+//   );
+//   // app.get("/", (req, res) => {
+//   //   res.send("API is running..");
+//   // });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
+// ///deploy
 
 app.get('/', (req, res) => {
   res.send('Hello World');
